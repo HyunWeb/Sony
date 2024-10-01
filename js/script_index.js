054e65505269d3915204c7bc0778ab9afea79029
HyunWeb <mattew1803@naver.com>
2024-10-01 13:59:42 +0900

    html css 완료js 시작 및 css 애니메이션 조정작업중

---
+++ js/script_index.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function () {
+  // let decoBall = this.document.querySelectorAll("#mainPage > img");
+  // this.window.setInterval(function () {
+  //   decoBall[0].style.transform = "translate(0, 100px)";
+  // }, 400);
+  // log(decoBall);
+
+  // this.document.addEventListener("scroll", function () {
+  //   let data = document.documentElement.scrollTop;
+  //   console.log(data);
+  // });
+
+  function log(data) {
+    return console.log(data);
+  }
+});
