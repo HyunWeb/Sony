9981098a7a898e18400de7eb43a46a74f394fccd
LYJ <leeyounjin@naver.com>
2024-10-02 13:53:09 +0900

    leeyounjin

--- js/skill.js
+++ js/skill.js
@@ -4,10 +4,16 @@ window.addEventListener("load", function () {
   function handleScroll() {
     boxes.forEach((box) => {
       const boxTop = box.getBoundingClientRect().top;
-      const triggerPoint = window.innerHeight / 1.2;
+      const boxBottom = box.getBoundingClientRect().bottom;
+      const triggerPoint = window.innerHeight / 1.3;
+      const hidePoint = window.innerHeight / 5;
 
       if (boxTop < triggerPoint) {
         box.classList.add("visible");
+      }
+
+      if (boxBottom < hidePoint || boxTop > window.innerHeight) {
+        box.classList.remove("visible");
       }
     });
   }
