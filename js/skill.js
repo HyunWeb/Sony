6da424eab6cec1d631481349762a01fdee567435
LYJ <leeyounjin@naver.com>
2024-10-04 22:25:36 +0900

    leeyounjin

--- js/skill.js
+++ js/skill.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function () {
   const boxes = document.querySelectorAll(".box");
+  const productImages = document.querySelectorAll(".product-image");
 
   function handleScroll() {
     boxes.forEach((box) => {
@@ -16,6 +17,22 @@ window.addEventListener("load", function () {
         box.classList.remove("visible");
       }
     });
+
+    productImages.forEach((image) => {
+      const imageTop = image.getBoundingClientRect().top;
+      const imageBottom = image.getBoundingClientRect().bottom;
+      const triggerPoint = window.innerHeight / 1.3;
+      const hidePoint = window.innerHeight / 5;
+
+      if (imageTop < triggerPoint) {
+        image.classList.add("visible");
+      }
+
+      if (imageBottom < hidePoint || imageTop > window.innerHeight) {
+        image.classList.remove("visible");
+      }
+    });
   }
+
   window.addEventListener("scroll", handleScroll);
 });
